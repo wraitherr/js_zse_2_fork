3d5bb5c4b6e3054a7cd1ad934ee754e702e8b55b
wraitherr <labatonih@gmail.com>
2025-10-09 10:10:54 +0200

    Add files via upload

---
+++ z2_boch.js
@@ -0,0 +1,3 @@
+document.write("ostatnia modyfikacja strony".fontcolor("blue").bold().fontsize(5).strike()+"<br>"); 
+document.write(document.lastModified.fontcolor("#0000FF").bold().strike().fontsize(5));
+
